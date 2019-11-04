4c62144de187ffd1695dda873f00912a2456818b
rohitravi26 <rohitravi26@gmail.com>
2019-11-04 11:21:35 +0530

    adding entity and controller class

--- currency-exchange-service/src/main/java/com/rohit26/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/rohit26/controller/CurrencyExchangeController.java
@@ -1,4 +1,21 @@
 package com.rohit26.controller;
 
+import com.rohit26.entity.ExchangeValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigInteger;
+
+@RestController
 public class CurrencyExchangeController {
+    @GetMapping("/currency-exchange/from/{src}/to/{dest}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String src,@PathVariable String dest)
+    {
+
+
+        return new ExchangeValue();
+    }
+
+
 }
--- currency-exchange-service/src/main/java/com/rohit26/entity/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/rohit26/entity/ExchangeValue.java
@@ -1,4 +1,73 @@
 package com.rohit26.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigInteger;
+
+@Entity
+@Table(name="exchange_value")
 public class ExchangeValue {
-}
+
+        @Id
+        private Long id;
+        private String frm;
+        private String to;
+        @Column(name = "conversion_multiple")
+        private BigInteger conversionMultiple;
+        private int port;
+        public ExchangeValue() {
+
+        }
+
+        public ExchangeValue(Long id, String frm, String to, BigInteger conversionMultiple) {
+            this.id = id;
+            this.frm = frm;
+            this.to = to;
+            this.conversionMultiple = conversionMultiple;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFrm() {
+            return frm;
+        }
+
+        public void setFrm(String frm) {
+            this.frm = frm;
+        }
+
+        public String getTo() {
+            return to;
+        }
+
+        public void setTo(String to) {
+            this.to = to;
+        }
+
+        public BigInteger getConversionMultiple() {
+            return conversionMultiple;
+        }
+
+        public void setConversionMultiple(BigInteger conversionMultiple) {
+            this.conversionMultiple = conversionMultiple;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+
+    }
+
